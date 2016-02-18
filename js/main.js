da8bb9dda4d2501c41fa284def186bdf57b20746
poopsplat <matt.m.soria@gmail.com>
2016-02-17 23:57:45 -0600

    Adding ajax example to form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-  // do your js stuff here brothers and sisters!
+
+  var $contactForm = $('#contactForm');
+
+  $contactForm.submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: '//formspree.io/matt.m.soria@gmail.com',
+      method: 'POST',
+      data: $(this).serialize(),
+      dataType: 'json',
+      beforeSend: function() {
+        $contactForm.prepend('<div class="alert alert-loading"><p>Sending message…</p></div>');
+      },
+      success: function(data) {
+        $contactForm.addClass('form-sent').find('.alert').removeClass('alert-loading').addClass('alert-sucess').html('<p>Thanks! Your message has been sent!</p>');
+      },
+      error: function(err) {
+        $contactForm.find('.alert').removeClass('alert-loading').addClass('alert-error').html('<p>Ops, there was an error.</p>');
+      }
+    });
+  });
 });
